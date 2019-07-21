b70686562601ae765a587f62eb8dc035cf35a680
Inna Didukh <infarmation@gmail.com>
2019-07-21 21:46:31 +0300

    edited code

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function squareEquation() {
 	var b = document.getElementById("b").value;
 	var c = document.getElementById("c").value;
 	var x1 = "x1 = ";
- 		var d = b * b - 4 * a * c;
+ 	var d = b * b - 4 * a * c;
  	if (d > 0) {
  	 	x1 += ((-b) + Math.sqrt(d))/(2*a);
- 		x1+="; x2= ";
- 		x1  += ((-b) - Math.sqrt(d))/(2*a);
+ 		x1 +="; x2= ";
+ 		x1 += ((-b) - Math.sqrt(d))/(2*a);
  	} else if (d == 0) {
   	    x1 = ((-b) + Math.sqrt(d))/(2*a);
     } else 
-  	    x1= "D < 0, no roots";
+  	    x1 = "D < 0, no roots";
    	document.getElementById("result").innerHTML = x1;
 }
-document.getElementById("btnSubmit").onclick = squareEquation;
+	document.getElementById("btnSubmit").onclick = squareEquation;
 
 
 
