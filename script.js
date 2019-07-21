a6b0df2d43f3621d2590fedc5bff8e10071fb3f5
Inna Didukh <infarmation@gmail.com>
2019-07-21 21:57:13 +0300

    changed var x1 to var x

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function squareEquation() {
 	var a = document.getElementById("a").value;
 	var b = document.getElementById("b").value;
 	var c = document.getElementById("c").value;
-	var x1 = "x1 = ";
+	var x = "x1 = ";
  	var d = b * b - 4 * a * c;
  	if (d > 0) {
- 	 	x1 += ((-b) + Math.sqrt(d))/(2*a);
- 		x1 +="; x2= ";
- 		x1 += ((-b) - Math.sqrt(d))/(2*a);
+ 	 	x += ((-b) + Math.sqrt(d))/(2*a);
+ 		x +="; x2= ";
+ 		x += ((-b) - Math.sqrt(d))/(2*a);
  	} else if (d == 0) {
-  	    x1 = ((-b) + Math.sqrt(d))/(2*a);
+  	    x = ((-b) + Math.sqrt(d))/(2*a);
     } else 
-  	    x1 = "D < 0, no roots";
-   	document.getElementById("result").innerHTML = x1;
+  	    x = "D < 0, no roots";
+   	document.getElementById("result").innerHTML = x;
 }
 	document.getElementById("btnSubmit").onclick = squareEquation;
 
